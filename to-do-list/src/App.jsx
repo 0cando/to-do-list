2af74db7b67f361a0067a839853db803f14fad0e
ocando <luidxq@gmail.com>
2023-11-01 19:23:24 -0400

    Terminado

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,34 +1,98 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { v4 as uuidv4} from 'uuid'
+import { MagicMotion } from "react-magic-motion";
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tareaInput, setTareaInput] = useState('')
+  const [tareas, setTareas] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setTareaInput(e.target.value)
+  }
+  
+  const anadirTareas = (e) => {
+    e.preventDefault();
+      if( tareaInput !== ''){
+          const nuevaTarea = [{
+          id: uuidv4(),
+          contenido: tareaInput,
+          completada: false
+        }, ...tareas]
+        setTareas(nuevaTarea)
+        setTareaInput('')
+      } else {
+        alert(`No puedes ingresar tareas vacias. Escribe algo.`)
+      }
+  }
+
+  const eliminarTarea = id => {
+    const nuevasTareas = tareas.filter( tarea => tarea.id !== id)
+    setTareas(nuevasTareas);
+  }
+
+  const completarTarea = id => {
+    const nuevasTareas = tareas.map ( tarea => {
+      if (tarea.id === id){
+        return {...tarea, completada: true}
+      }
+      return tarea;
+    })
+    setTareas(nuevasTareas)
+  }
 
   return (
-    <>
+
+    <div className='g-cont'>
+
+      <h1>Lista de tareas.</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form className='formulario'>
+          <input
+            placeholder='Sacar a pasear el perro, hacer la cama...'
+            onChange={handleChange}
+            value={tareaInput}
+            />
+          <button
+          onClick={anadirTareas}>
+            ➕
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <MagicMotion>
+        <div className='tarea-list'>
+            {
+              tareas.map( (tarea) => (
+                <li 
+                key={tarea.id}
+                className={tarea.completada ? "tarea-completada" : null}
+                >
+                  {tarea.contenido}
+                  <div className='btn-tareas'>
+                    { tarea.completada
+                      ?
+                      <button
+                        onClick={ () => eliminarTarea(tarea.id)}
+                      >
+                        ❌
+                      </button>
+                      :
+                      <button
+                        onClick={ () => completarTarea(tarea.id)}
+                      >
+                        ✅
+                      </button>
+                    }
+                  </div>
+                </li>
+              ))
+            }
+        </div>
+      </MagicMotion>
+    </div>
   )
 }
 
